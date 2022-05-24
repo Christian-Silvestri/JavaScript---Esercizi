c9244907ce6b165aa0b154fc982e6b286c6b6901
Christian-Silvestri <christiansilvestri@live.it>
2022-05-24 22:31:10 +0200

    JavaScript-Exercise

---
+++ ES-23/exercise-1.js
@@ -0,0 +1,9 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+console.log(Object.keys(person)[0]);
+console.log(Object.keys(person)[1]);
+console.log(Object.keys(person)[2]);
